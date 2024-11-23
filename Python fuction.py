ce4a4aa18a5d87e56b918acc8c656e90b1388ef2
Praveen15082004 <praveensanjay2004@gmail.com>
2024-11-23 11:32:45 +0530

    Add files via upload

---
+++ Python fuction.py
@@ -0,0 +1,66 @@
+
+def add():
+    print("Addtion")
+    a=int(input("enter a:"))
+    b=int(input("enter b:"))
+    print(a+b)
+    
+def sub():
+     print("Subration")
+     a=int(input("enter a:"))
+     b=int(input("enter b:"))
+     print(a-b)
+
+def div():
+    print("Division")
+    a=int(input("enter a:"))
+    b=int(input("enter b:"))
+    print(a%b)
+
+def mul():
+    print("Multipication")
+    a=int(input("enter a:"))
+    b=int(input("enter b:"))
+    print(a*b)
+
+mul()
+div()
+sub()
+add()
+
+#Odd or Even...
+
+'''def findoddoreven(b):
+    if(b%2==0):
+        print("Even")
+    else:
+        print("odd")
+a=int(input("Enter the num:"))
+findoddoreven(a)'''
+
+#Pass or Fail..
+
+'''def findpassorfail(msg):
+    if(msg>=35):
+        print("Pass")
+    else:
+        print("Fail")
+b=int(input("Enter the Mark:"))
+findpassorfail(b)'''
+
+#Print Range..
+
+'''def printrange():
+    for i in range(1,23):
+        print(i)
+printrange()
+
+
+def printrange(a,b):
+    for i in range(a,b):
+        print(i)
+a=int(input("Enter the number:"))
+b=int(input("Enter the number:"))
+printrange(a,b)'''
+
+
