02710fab081ea43641dc6bd7fa1537ea861032d0
Praveen15082004 <praveensanjay2004@gmail.com>
2024-11-23 11:32:15 +0530

    Add files via upload

---
+++ const & self key.py
@@ -0,0 +1,60 @@
+'''class laptop:
+    def __init__(self):
+        self.price=0
+    
+        self.Ram=""
+        self.processor=""
+    def display(self):
+        print("price=",self.price)
+        print("Ram=",self.Ram)
+        print("processer=",self.processor)
+hp=laptop()
+dell=laptop()
+
+hp.price=50000
+hp.Ram="5gb"
+hp.processor="i7"
+
+dell.price=60000
+dell.Ram="16GB"
+dell.processor="i12"
+
+dell.display()
+hp.display()'''
+
+# student 
+class student:
+    def __init__(self):
+        self.Name="praveen"
+        self.Register="58"
+    def display(self):
+        print("Name:",self.Name)
+        print("Register:",self.Register)
+std1=student()
+std2=student()
+
+std1.Name="nithish"
+std1.Register=1
+
+std2.Name="Lokesh"
+std2.Register=9
+
+std1.display()
+std2.display()
+
+# fruits
+'''class fruits:
+    def __init__(self,col):
+        self.color=col
+apple=fruits("red")
+orange=fruits("orange")
+
+print(apple.color)
+print(orange.color)
+
+# Teacher
+
+class Teacher:
+    def __init__(self,Name,Reg):
+        self.Name="N "
+        self.'''
